c2de7ed214725a442c30582c59f23d4b8ac1d358
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-05-30 09:46:10 +0530

    Quartz sync: May 30, 2024, 9:46 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "arkamfahry.github.io/knowledge-base",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
