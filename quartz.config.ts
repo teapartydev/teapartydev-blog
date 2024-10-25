994e5d6e1b531fd6cae7b20b409be1878f6902a2
driftdev <92285161+driftdev@users.noreply.github.com>
2024-10-25 22:12:08 +0530

    Quartz sync: Oct 25, 2024, 10:12 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "TeaPartyDev ☕🎉",
+    pageTitle: "TeaPartyDev",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
